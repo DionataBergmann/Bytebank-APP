e2236d5801fbd24d5e69beb38ad0a202ba91e81f
DionataBergmann <dionata.podewils@outlook.com>
2025-12-05 15:58:06 -0300

    :sparkles: feat(domain): Create IDashboardRepository interface

---
+++ src/domain/repositories/IDashboardRepository.ts
@@ -0,0 +1,15 @@
+import { DashboardData, ChartData } from '../entities/Dashboard';
+
+export interface IDashboardRepository {
+  getDashboardData(
+    userId: string,
+    period: 'week' | 'month' | 'year',
+    selectedMonth: string
+  ): Promise<DashboardData>;
+  getChartData(
+    userId: string,
+    period: 'week' | 'month' | 'year',
+    selectedMonth: string
+  ): Promise<ChartData[]>;
+}
+
